1b87b933ff98a1de225cd2300ecccfc512bf8c19
Miguel Collado <m.collado.gomez@gmail.com>
2020-10-19 12:10:02 +0200

    Actualizar api.dart, entities.dart y 12 más archivos...

---
+++ lib/communications/data/push_notifications.dart
@@ -0,0 +1,13 @@
+// Isolate here firebase
+class PushNotifications {
+
+}
+
+PushNotifications getPushNotifications() {
+  return _FirebasePushNotifications();
+}
+
+
+class _FirebasePushNotifications implements PushNotifications {
+
+}
---
+++ lib/shared/data/storage.dart
@@ -0,0 +1,13 @@
+// isolate here the concrete plugin used for storage
+class LocalStorage {
+
+}
+
+LocalStorage getLocalStorage() {
+  return _FlutterLocalStorage();
+}
+
+
+class _FlutterLocalStorage implements LocalStorage {
+
+}
