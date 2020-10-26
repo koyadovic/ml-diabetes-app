9b9e6fda6f8c04a4ae036e0f2841d63b49658ad7
Miguel Collado <m.collado.gomez@gmail.com>
2020-10-26 07:54:12 +0100

    Actualizar theme.dart

--- lib/shared/view/utils/theme.dart
+++ lib/shared/view/utils/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class DiaTheme {
-  static final Color primaryColor = Colors.teal;
-  static final Color primarySwatch = Colors.teal;
+  static final Color primaryColor = Colors.indigo;
+  static final Color primarySwatch = Colors.indigo;
 }
