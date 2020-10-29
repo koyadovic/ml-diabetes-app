f0e28639c347098081f6d0773e437a9b6b170148
Miguel Collado <m.collado.gomez@gmail.com>
2020-10-29 05:58:18 +0100

    Actualizar entities.dart

--- lib/communications/model/entities.dart
+++ lib/communications/model/entities.dart
@@ -1,12 +1,52 @@
-class DiaMessage {
+class Message {
+  final int id;
+  final DateTime createdDate;
+  final String type;
   final String title;
-  final String subtitle;
-  final Map<String, dynamic> data;
+  final String text;
+  final Map<String, dynamic> payload;
 
-  DiaMessage({this.title, this.subtitle, this.data});
+  Message({this.id, this.createdDate, this.type, this.title, this.text, this.payload});
 
-  String toString() {
-    return this.title + ': ' + this.subtitle + ', ' + this.data.toString();
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      id: json['id'],
+      createdDate: DateTime.fromMicrosecondsSinceEpoch(json['created_date'] * 1000000),
+      type: json['type'],
+      title: json['title'],
+      text: json['text'],
+      payload: json['payload'],
+    );
   }
 
+  String toString() {
+    return this.title + ': ' + this.text + ', ' + this.payload.toString();
+  }
+}
+
+
+class FeedbackRequest {
+  final int id;
+  final DateTime createdDate;
+  final DateTime deliveryDate;
+  final String title;
+  final String text;
+  final List<String> options;
+
+  FeedbackRequest({this.id, this.createdDate, this.deliveryDate, this.title, this.text, this.options});
+
+  factory FeedbackRequest.fromJson(Map<String, dynamic> json) {
+    return FeedbackRequest(
+      id: json['id'],
+      createdDate: DateTime.fromMicrosecondsSinceEpoch(json['created_date'] * 1000000),
+      deliveryDate: DateTime.fromMicrosecondsSinceEpoch(json['delivery_date'] * 1000000),
+      title: json['title'],
+      text: json['text'],
+      options: List<String>.from(json['options']),
+    );
+  }
+
+  String toString() {
+    return this.title + ': ' + this.text + ', ' + this.options.toString();
+  }
 }
