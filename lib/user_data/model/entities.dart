501bbd4e0601b819a7028adc0c4f65438ac12209
Miguel Collado <m.collado.gomez@gmail.com>
2020-11-01 06:45:36 +0100

    Actualizar entities.dart

--- lib/user_data/model/entities.dart
+++ lib/user_data/model/entities.dart
@@ -79,7 +79,7 @@ class GlucoseLevel extends UserDataEntity {
       eventDate: DateTime.fromMicrosecondsSinceEpoch((json['event_date'] * 1000000.0).round()),
       userId: json['user_id'],
       entityType: json['entity_type'],
-      level: (json['level'] as double).toInt(),
+      level: json['level'].toInt(),
     );
   }
 
