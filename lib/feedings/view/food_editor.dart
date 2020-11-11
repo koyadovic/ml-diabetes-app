3696fcabed4ff90cd9bbca984acf588c430f7f59
Miguel Collado <m.collado.gomez@gmail.com>
2020-11-11 18:17:43 +0100

    Actualizar food_editor.dart y food_selection.dart

---
+++ lib/feedings/view/food_editor.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FoodEditorWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FoodEditorWidgetState();
+  }
+}
+
+
+class FoodEditorWidgetState extends State<FoodEditorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
